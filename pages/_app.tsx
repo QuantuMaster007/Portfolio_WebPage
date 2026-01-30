1a298507c3ca08e8b5fb18f42629d163dcb6132a
Sourabh232 <sourabh232@gmail.com>
2026-01-29 16:23:30 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,35 +11,11 @@ function handleExitComplete() {
 
 function MyApp({ Component, pageProps }: any) {
   const router = useRouter()
-  const scrollRef = React.useRef<HTMLDivElement>(null)
-
-  // React.useEffect(() => {
-  //   import('locomotive-scroll').then(locomotiveModule => {
-  //     // @ts-ignore
-  //     const scroll = new locomotiveModule.default({
-  //       el: scrollRef.current,
-  //       smooth: true,
-  //       inertia: 0.5,
-  //     })
-
-  //     scroll.smartphone.smooth = true
-      // console.log()
-      // setTimeout(() => {
-      //   scroll.on('scroll', (obj: any) => {
-      //     scroll.update()
-      //     console.log(obj)
-      //   })
-      //   scroll.update()
-      // }, 1000)
-    // })
-  // }, [])
 
   return (
-    <div ref={scrollRef}>
-      <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </div>
+    <AnimatePresence mode="wait" onExitComplete={handleExitComplete}>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
   )
 }
 
